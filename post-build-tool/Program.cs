8daafb1380a6f80d916de76f1eb7967f9f5eb89d
Nathaniel Moschkin <theimprobable@hotmail.com>
2022-12-26 23:27:45 -0500

    Laying the ground work

--- post-build-tool/Program.cs
+++ post-build-tool/Program.cs
@@ -1,2 +1,388 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using System.Xml;
+
+namespace PostBuildTool
+{
+    public static class Program
+    {
+        public class CommandSwitch : ICloneable
+        {
+            public static string ProgramTitle { get; } = "post-build-tool";
+
+            public static List<CommandSwitch> Commands { get; }
+
+            public static CommandSwitch FindCommand(string arg)
+            {
+                arg = arg.Trim().ToLower();
+                return Commands.FirstOrDefault(x => x.Switches.Contains(arg));
+            }
+
+            static CommandSwitch()
+            {
+                Commands = MakeSwitches();
+            }
+
+            public static List<CommandSwitch> MakeSwitches()
+            {
+                var cmds = new List<CommandSwitch>
+                {
+                    new CommandSwitch("/d|/dir", "The directory to start scanning for .csproj files to update.", "dir"),
+                    new CommandSwitch("/r|/recursive", "Recursively scan the directory for .csproj files."),
+                    new CommandSwitch("/v|/version", "Force the specified version number (as opposed to auto-incrementing.)", "version"),
+                    new CommandSwitch("/ov|/oldversion", "Specify the PreviousVersion variable (as opposed to being automatically calculated.)", "version"),
+                    new CommandSwitch("/q|/quiet", "Suppress all output."),
+                    new CommandSwitch("/l|/log", "Log output to the specified file.", "file"),
+                    new CommandSwitch("/h|/help", "Displays this help screen.")
+                };
+                return cmds;
+            }
+
+            public string ShortName { get; set; }
+
+            public string Description { get; set; }
+
+            public List<string> Switches { get; set; }
+
+            public bool TakesArgument { get; set; }
+
+            public string ArgumentName { get; set; }
+
+            public bool Required { get; set; }
+
+            public bool IsSet { get; set; }
+
+            public string ArgumentValue { get; set; }
+
+            object ICloneable.Clone() => Clone();
+
+            public CommandSwitch Clone()
+            {
+                return (CommandSwitch)MemberwiseClone();
+            }
+
+            public static Dictionary<string, CommandSwitch> ParseArgs(IList<string> args)
+            {
+                var dict = new Dictionary<string, CommandSwitch>();
+
+                int i, c = args.Count;
+
+                for (i = 0; i < c; i++)
+                {
+                    var arg = args[i];
+
+                    var farg = FindCommand(arg);
+
+                    if (farg == null)
+                    {
+                        PrintHelp();
+                        return default;
+                    }
+
+                    farg = farg.Clone();
+
+                    if (arg.StartsWith("/"))
+                    {
+                        if (dict.ContainsKey(farg.ShortName))
+                        {
+                            Console.WriteLine("Duplicated Command Arguments");
+                        }
+
+                        if (i < c - 1 && !args[i + 1].StartsWith("/"))
+                        {
+                            var dir = args[i + 1];
+                            farg.ArgumentValue = dir;
+                            i++;
+                        }
+
+                        farg.IsSet = true;
+                        dict.Add(farg.ShortName, farg);
+                    }
+                }
+
+                return dict;
+            }
+
+            public CommandSwitch(string[] switches, string desc, bool takesArgument, string argumentName, bool required = false)
+            {
+                bool noswitch;
+                if (switches == null || switches.Length == 0)
+                {
+                    Switches = new List<string>();
+                    noswitch = true;
+                }
+                else
+                {
+                    Switches = new List<string>(switches);
+                    noswitch = false;
+                }
+
+                Description = desc;
+                TakesArgument = takesArgument;
+                ArgumentName = argumentName;
+                Required = required;
+
+                if (noswitch && ((!TakesArgument) || string.IsNullOrEmpty(argumentName)))
+                {
+                    throw new InvalidOperationException("If you do not specify a switch, you must specify an argument name.");
+                }
+
+                if (noswitch)
+                {
+                    ShortName = argumentName;
+                }
+                else
+                {
+                    Switches.Sort((a, b) =>
+                    {
+                        return a.Length - b.Length;
+                    });
+
+                    ShortName = Switches[0];
+                }
+            }
+
+            public CommandSwitch(string switches, string desc, string argumentName, bool required = false) : this(switches.Split('|'), desc, true, argumentName, required)
+            {
+            }
+
+            public CommandSwitch(string switches, string desc, bool required = false) : this(switches.Split('|'), desc, false, null, required)
+            {
+            }
+
+            public override string ToString()
+            {
+                if (Switches == null || Switches.Count == 0)
+                {
+                    if (Required)
+                    {
+                        return $"<{ArgumentName}>";
+                    }
+                    else
+                    {
+                        return $"[<{ArgumentName}>]";
+                    }
+                }
+                else
+                {
+                    var sw = string.Join("|", Switches);
+                    string arg = "";
+
+                    if (!string.IsNullOrEmpty(ArgumentName)) arg = $" <{ArgumentName}>";
+
+                    if (Required)
+                    {
+                        return $"{sw}{arg}";
+                    }
+                    else
+                    {
+                        return $"[{sw}{arg}]";
+                    }
+                }
+            }
+
+            public static void PrintHelp(bool exit = true)
+            {
+                Console.WriteLine("post-build-tool v1.0");
+                Console.WriteLine();
+                Console.WriteLine("Copyright (C) 2023 Nathaniel Moschkin");
+                Console.WriteLine("All Rights Reserved");
+                Console.WriteLine();
+                Console.WriteLine("Usage:");
+                var ptstr = $"    {ProgramTitle} ";
+                Console.Write(ptstr);
+
+                int col = ptstr.Length;
+                foreach (var cmd in Commands)
+                {
+                    var sc = cmd.ToString();
+
+                    if (col + sc.Length > 80)
+                    {
+                        Console.Write("\r\n" + "".PadRight(ptstr.Length));
+                        col = ptstr.Length;
+                    }
+
+                    Console.Write(" " + sc);
+                    col += sc.Length;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine();
+
+                var maxname = Commands.Max(x => x.Switches.Max(y => y.Length)) + 7;
+                var arglen = Commands.Max(x => x.ArgumentName?.Length ?? 0) + 10;
+
+                foreach (var cmd in Commands)
+                {
+                    int i, c = cmd.Switches?.Count ?? 0;
+
+                    Console.Write($"    {cmd.Switches[0]}    ".PadRight(maxname));
+
+                    if (!string.IsNullOrEmpty(cmd.ArgumentName))
+                    {
+                        Console.Write($"    <{cmd.ArgumentName}> ".PadRight(arglen));
+                    }
+                    else
+                    {
+                        Console.Write($"{"".PadRight(arglen)}");
+                    }
+
+                    Console.WriteLine(cmd.Description);
+
+                    for (i = 1; i < c; i++)
+                    {
+                        Console.WriteLine($"    {cmd.Switches[i]}");
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine();
+
+                Console.WriteLine();
+
+                if (exit) Environment.Exit(0);
+            }
+        }
+
+        private static void ProcessProject(string file)
+        {
+            FConsole.WriteLine($"Processing file '{Path.GetFileName(file)}' ...");
+
+            var txt = File.ReadAllText(file);
+
+            var doc = new XmlDocument();
+
+            doc.LoadXml(txt);
+
+            XmlNode proj = doc["Project"];
+        }
+
+        private static void ProcessFolder(string folder)
+        {
+            FConsole.WriteLine($"Scanning folder '{folder}' ...");
+
+            var abspath = Path.GetFullPath(folder);
+
+            var files = Directory.GetFiles(abspath, "*.csproj");
+            var dirs = Directory.GetDirectories(abspath);
+
+            foreach (var file in files)
+            {
+                ProcessProject(file);
+            }
+
+            foreach (var dir in dirs)
+            {
+                ProcessFolder(dir);
+            }
+        }
+
+        public static bool Silent { get; private set; }
+        public static FileConsole FConsole { get; private set; }
+        public static Dictionary<string, CommandSwitch> RunArgs { get; private set; }
+
+        public static void Main(string[] args)
+        {
+            var folder = Path.GetFullPath(".");
+
+            var parsed = CommandSwitch.ParseArgs(args);
+
+            RunArgs = parsed;
+
+            //if (parsed == null || parsed.Count == 0)
+            //{
+            //    CommandSwitch.PrintHelp(true);
+            //    return;
+            //}
+
+            if (parsed.TryGetValue("/d", out var fldarg))
+            {
+                folder = fldarg.ArgumentValue;
+            }
+
+            Silent = parsed.ContainsKey("/q");
+
+            if (parsed.TryGetValue("/l", out var logarg))
+            {
+                FConsole = new FileConsole(Silent, logarg.ArgumentValue);
+            }
+            else
+            {
+                FConsole = new FileConsole(Silent);
+            }
+
+            FConsole.WriteLine("post-build-tool v1.0");
+            FConsole.WriteLine();
+            FConsole.WriteLine("Copyright (C) 2023 Nathaniel Moschkin");
+            FConsole.WriteLine("All Rights Reserved");
+            FConsole.WriteLine();
+
+            ProcessFolder(folder);
+        }
+    }
+
+    public class FileConsole : IDisposable
+    {
+        private FileStream fs = null;
+        private bool disposedValue;
+
+        public bool Silent { get; }
+
+        public FileConsole(bool silent, string filename)
+        {
+            Silent = silent;
+            this.fs = new FileStream(filename, FileMode.Create, FileAccess.Write, FileShare.Read);
+        }
+
+        public FileConsole(bool silent)
+        {
+            Silent = silent;
+        }
+
+        public FileConsole() : this(false)
+        {
+        }
+
+        public void Write(object obj = null)
+        {
+            if (!Silent) Console.Write(obj);
+            fs?.Write(Encoding.UTF8.GetBytes(obj.ToString()));
+        }
+
+        public void WriteLine(object obj = null)
+        {
+            if (!Silent) Console.WriteLine(obj);
+            fs?.Write(Encoding.UTF8.GetBytes(obj.ToString() + "\r\n"));
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    fs?.Dispose();
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~FileConsole()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ post-build-tool/Project.cs
@@ -0,0 +1,166 @@
+﻿using Newtonsoft.Json;
+
+using System;
+using System.Linq;
+using System.Text;
+
+namespace post_build_tool
+{
+    public class Version : IEquatable<Version>
+    {
+        public int Major { get; set; }
+
+        public int Minor { get; set; }
+
+        public int Revision { get; set; }
+
+        public int? Build { get; set; } = null;
+
+        public static int GetHourOfYear()
+        {
+            var d = DateTime.Now.DayOfYear * 24;
+            d += DateTime.Now.Hour;
+
+            return d;
+        }
+
+        public static int GetMinuteOfDay()
+        {
+            return (int)DateTime.Now.TimeOfDay.TotalMinutes;
+        }
+
+        public bool Equals(Version other)
+        {
+            return Major == other.Major && Minor == other.Minor && Revision == other.Revision && Build == other.Build;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Version v) return Equals(v);
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return (Major, Minor, Revision, Build).GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            if (Build != null)
+            {
+                return $"{Major}.{Minor}.{Revision}.{Build}";
+            }
+            else
+            {
+                return $"{Major}.{Minor}.{Revision}";
+            }
+        }
+
+        public static Version Parse(string version)
+        {
+            var sp = version.Split('.');
+            var v = new Version();
+
+            if (sp.Length >= 1)
+            {
+                v.Major = int.Parse(sp[0]);
+            }
+
+            if (sp.Length >= 2)
+            {
+                v.Minor = int.Parse(sp[1]);
+            }
+
+            if (sp.Length >= 3)
+            {
+                v.Revision = int.Parse(sp[2]);
+            }
+
+            if (sp.Length >= 4)
+            {
+                v.Build = int.Parse(sp[3]);
+            }
+
+            return v;
+        }
+
+        public Version()
+        { }
+
+        public Version(int major, int minor, int revision)
+        {
+            Major = major;
+            Minor = minor;
+            Revision = revision;
+        }
+
+        public Version(int major, int minor, int revision, int build)
+        {
+            Major = major;
+            Minor = minor;
+            Revision = revision;
+            Build = build;
+        }
+    }
+
+    public class Project
+    {
+        [JsonProperty("TargetFramework")]
+        public string TargetFramework { get; set; }
+
+        [JsonProperty("ImplicitUsings")]
+        public string ImplicitUsings { get; set; }
+
+        [JsonProperty("Nullable")]
+        public string Nullable { get; set; }
+
+        [JsonProperty("AssemblyVersion")]
+        public Version AssemblyVersion { get; set; }
+
+        [JsonProperty("FileVersion")]
+        public Version FileVersion { get; set; }
+
+        [JsonProperty("Authors")]
+        public string Authors { get; set; }
+
+        [JsonProperty("PreviousVersion")]
+        public Version PreviousVersion { get; set; }
+
+        [JsonProperty("Version")]
+        public Version Version { get; set; }
+
+        [JsonProperty("Product")]
+        public string Product { get; set; }
+
+        [JsonProperty("Description")]
+        public string Description { get; set; }
+
+        [JsonProperty("Copyright")]
+        public string Copyright { get; set; }
+
+        [JsonProperty("RepositoryUrl")]
+        public string RepositoryUrl { get; set; }
+
+        [JsonProperty("RepositoryType")]
+        public string RepositoryType { get; set; }
+
+        [JsonProperty("NeutralLanguage")]
+        public string NeutralLanguage { get; set; }
+
+        [JsonProperty("SignAssembly")]
+        public string SignAssembly { get; set; }
+
+        [JsonProperty("GeneratePackageOnBuild")]
+        public string GeneratePackageOnBuild { get; set; }
+
+        [JsonProperty("Title")]
+        public string Title { get; set; }
+
+        [JsonProperty("PackageLicenseExpression")]
+        public string PackageLicenseExpression { get; set; }
+
+        [JsonProperty("PackageRequireLicenseAcceptance")]
+        public string PackageRequireLicenseAcceptance { get; set; }
+    }
+}
